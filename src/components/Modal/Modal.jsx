adb27263f4c51b31a71107ea49cb9250e3d4d74c
Fenickss <denis.suxov.1991@mail.ru>
2023-06-15 22:54:58 +0300

    add

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,33 @@
 import PropTypes from "prop-types";
-
+import { createPortal } from "react-dom";
+import { useEffect } from "react";
 import s from "../Modal/Modal.module.css";
+import { useDispatch } from "react-redux";
+import { closeModal } from "../../redux/modal/modalSlice";
 
-const Modal = ({ children }) => {
-  return (
+const modalRoot = document.querySelector("#modal-root");
+
+const Modal = ({ children, handleKeyDown }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
+  handleKeyDown = (e) => {
+    if (e.code === "Escape") {
+      console.log("escape redux");
+      dispatch(closeModal());
+    }
+  };
+
+  return createPortal(
     <div className={s.modal__backDrop}>
       <div className={s.modal__content}>{children}</div>
-    </div>
+    </div>,
+    modalRoot
   );
 };
 
--- src/components/ShoppingListPage/ShoppingListPage.jsx
+++ src/components/ShoppingListPage/ShoppingListPage.jsx
@@ -1,9 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import s from "./ShoppingListPage.module.css";
 import ShoppingBalance from "../ShoppingBalance";
 import PriceConverter from "../PriceConverter/PriceConverter";
 import TodoList from "../TodoList";
 
+import { VscChromeClose } from "react-icons/vsc";
+import { MdOutlineClose } from "react-icons/ai";
+
+import Modal from "../Modal";
+import { openModal, closeModal } from "../../redux/modal/modalSlice";
+
 const ShoppingListPage = () => {
+  const dispatch = useDispatch();
+  const { isOpen } = useSelector((store) => store.modal);
   return (
     <>
       <div className={s.header}>
@@ -44,6 +54,26 @@ const ShoppingListPage = () => {
             <PriceConverter />
           </div>
           <TodoList />
+          <button
+            className={s.button__openModal}
+            type="submit"
+            onClick={() => dispatch(openModal())}
+          >
+            <VscChromeClose className={s.modal__iconAdd} />
+          </button>
+          {isOpen && (
+            <Modal>
+              <h1>TEST</h1>
+              <p>fsdf</p>
+              <button
+                className={s.button__closeModal}
+                type="submit"
+                onClick={() => dispatch(closeModal())}
+              >
+                <MdOutlineClose className={s.modal__iconClose} />
+              </button>
+            </Modal>
+          )}
         </div>
       </main>
     </>
